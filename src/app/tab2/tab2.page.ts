23c9465b2f34e2ea91689db270678a310a0422ce
kai <kai@kai-asus>
2019-11-06 10:51:48 +0800

    updated instructions tab

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  insImg1 = 'assets/img/ins1.PNG';
+  insImg2 = 'assets/img/ins2.PNG';
+  insImg3 = 'assets/img/ins3.PNG';
+  insImg4 = 'assets/img/ins4.PNG';
+  insImg5 = 'assets/img/ins5.PNG';
+  insImg6 = 'assets/img/ins6.PNG';
+  insImg7 = 'assets/img/ins7.PNG';
+  insImg8 = 'assets/img/ins8.PNG';
+  insImg9 = 'assets/img/ins9.PNG';
+  insImg10 = 'assets/img/ins10.PNG';
+  insImg11 = 'assets/img/ins11.PNG';
+  insImg12 = 'assets/img/ins12.PNG';
+  insImg13 = 'assets/img/ins13.PNG';
+  insImg14 = 'assets/img/ins14.PNG';
+  insImg15 = 'assets/img/ins15.PNG';
+  insImg16 = 'assets/img/ins16.PNG';
+  insImg17 = 'assets/img/ins17.PNG';
+  insImg18 = 'assets/img/ins18.PNG';
+  insImg19 = 'assets/img/ins19.PNG';
+  insImg20 = 'assets/img/ins20.PNG';
+
   constructor() {}
 
 }
